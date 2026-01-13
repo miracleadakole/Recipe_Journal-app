b77c8d1099f000d50e7c33f41c548af98679f72b
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2026-01-13 16:34:32 +0100

    remove hardcoded secret keys and load from .env file

--- Recipe_Journal/Recipe_Journal/settings.py
+++ Recipe_Journal/Recipe_Journal/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c%vkagu#@&*j@gzm^rxftyb!kv4&(*hc3(kl2%e+4=tr$7m3qa'
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = 'django-insecure-c%vkagu#@&*j@gzm^rxftyb!kv4&(*hc3(kl2%e+4=tr$7m3qa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
