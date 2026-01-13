3dd4ad50687cc73f271ed09f64989ebcbafecef1
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2026-01-13 19:01:31 +0100

    Deploy: configure Django app for Render

--- Recipe_Journal/Recipe_Journal/settings.py
+++ Recipe_Journal/Recipe_Journal/settings.py
@@ -30,9 +30,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECRET_KEY = 'django-insecure-c%vkagu#@&*j@gzm^rxftyb!kv4&(*hc3(kl2%e+4=tr$7m3qa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
 
 
 # Application definition
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -151,7 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
